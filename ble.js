a5b0e762a84ce9b3c1ce20ef75c433d3efc38062
Mikael Kindborg <mikael.kindborg@gmail.com>
2016-03-23 13:10:21 +0100

    Updated documentation comment.

--- ble.js
+++ ble.js
@@ -20,7 +20,8 @@ var exec = cordova.require('cordova/exec');
  * One or more service UUIDs must be specified for iOS background scanning to work.</p>
  * <p>Found devices and errors are reported to the supplied callback functions.</p>
  * <p>Will keep scanning until you call stopScan().</p>
- * <p>To conserve energy, call stopScan() as soon as you've found the device you're looking for.</p>
+ * <p>To conserve energy, call stopScan() as soon as you've found the device
+ * you're looking for.</p>
  * <p>Call stopScan() before calling startScan() again.</p>
  *
  * @param {array} uuids - Array with service UUID strings (optional).
@@ -33,7 +34,8 @@ var exec = cordova.require('cordova/exec');
  * is the same on Android and iOS. Learning out this parameter or
  * setting it to null, will scan for all devices, regardless of
  * advertised services.
- * @param {scanCallback} success - Success callback, called repeatedly for each found device.
+ * @param {scanCallback} success - Success callback, called repeatedly
+ * for each found device.
  * @param {failCallback} fail - Error callback.
  *
  * @example
@@ -49,16 +51,16 @@ var exec = cordova.require('cordova/exec');
  *       }
  *   );
  *
- *   // Scan for specific service (or services).
+ *   // Scan for specific service (Eddystone Service UUID).
  *   evothings.ble.startScan(
- *       ['0000ffff-0000-1000-8000-00805f9b34fb'],
+ *       ['0000FEAA-0000-1000-8000-00805F9B34FB'],
  *       function(device)
  *       {
- *       console.log('BLE startScan found device named: ' + device.name);
+ *           console.log('BLE startScan found device named: ' + device.name);
  *       },
  *       function(errorCode)
  *       {
- *       console.log('BLE startScan error: ' + errorCode);
+ *           console.log('BLE startScan error: ' + errorCode);
  *       }
  *   );
  */
